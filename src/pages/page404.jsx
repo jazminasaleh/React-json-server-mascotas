bfba563e7b690966adc818fff2923d2fda0d7c0d
jazmin saleh peña <jazminasaleh@gmail.com>
2023-06-06 15:58:03 -0500

    primer commit

---
+++ src/pages/page404.jsx
@@ -0,0 +1,12 @@
+import image from "../assets/img/doguito404.svg"
+import '../assets/css/404.css'
+const Page404 = () =>{
+    return(
+        <main className="container flex flex--center flex--column">
+            <img className="dog-image" src={image} alt="Doguito"></img>
+            <p className="notfount-text">Esta pagian no existe</p>
+        </main>
+    )
+}
+
+export default Page404
